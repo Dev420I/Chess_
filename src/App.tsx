c6da4fee7a20db18beb8e704d8159fcb0a5f9302
Dav420I <david.arutyunyan.97@bk.ru>
2022-08-30 19:06:34 +0300

    css commit 2.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css'
 import BoardComponents from "./components/BoardComponents";
+import {Board} from "./models/Board";
+
+
 const App = () => {
-  return (
-    <div className='app'>
-   <BoardComponents/>
-    </div>
-  );
+    const [board, setBoard] = useState(new Board())
+
+    useEffect(() => {
+        restart()
+    }, [])
+
+    function restart(){
+        const newBoard = new Board()
+        newBoard.initCell()
+        setBoard(newBoard)
+    }
+
+    return (
+        <div className='app'>
+            <BoardComponents
+                board={board}
+                setBoard={setBoard}
+            />
+        </div>
+    );
 };
 
 export default App;
