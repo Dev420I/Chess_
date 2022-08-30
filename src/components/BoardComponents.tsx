2d60637b53442bc0eb00eca5c1c1781145afe77d
Dav420I <david.arutyunyan.97@bk.ru>
2022-08-30 19:00:18 +0300

    board commit

--- src/components/BoardComponents.tsx
+++ src/components/BoardComponents.tsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, {FC} from 'react';
+import {Board} from "../models/Board";
+import CellComponents from "./CellComponents";
 
-const BoardComponents = () => {
+interface BoardProps {
+    board: Board;
+    setBoard: (board: Board) => void;
+}
+
+const BoardComponents: FC<BoardProps> = ({board, setBoard }) => {
 
     return (
         <div className='board'>
-
+            {board.cells.map((row, index) =>
+                <React.Fragment key={index}>
+                    {row.map(cell =>
+                        <CellComponents
+                            cell={cell}
+                            key={cell.id}
+                        />
+                    )}
+                </React.Fragment>
+            )}
         </div>
     );
 };
