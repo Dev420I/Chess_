b9fb2163cf50754cf15ca6e88055c7f9f5b99d21
Dav420I <david.arutyunyan.97@bk.ru>
2022-08-31 22:23:18 +0300

    Figure commit 3.0

--- src/models/Board.ts
+++ src/models/Board.ts
@@ -1,8 +1,6 @@
 import {Cell} from './Cell'
 import {Colors} from "./Colors";
-import {Queen} from "./figures/Queen";
 import {Pawn} from "./figures/Pawn";
-import {Bishop} from "./figures/Bishop";
 
 export class Board {
     cells: Cell[][] = []
@@ -26,14 +24,38 @@ export class Board {
     }
 
     private addPawns(){
-
+        for (let i = 0; i < 8; i++) {
+            new Pawn(Colors.BLACK, this.getCell(i,1))
+            new Pawn(Colors.WHITE, this.getCell(i,7))
+        }
     }
 
     private addKing(){
 
     }
 
-    public addFigure(){
+    private addQueen(){
 
+    }
+
+    private addKnights(){
+
+    }
+
+    private addRook(){
+
+    }
+
+    private addBishop(){
+
+    }
+
+    public addFigure(){
+        this.addBishop()
+        this.addRook()
+        this.addKnights()
+        this.addQueen()
+        this.addKing()
+        this.addPawns()
     }
 }
