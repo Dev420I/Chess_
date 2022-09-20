737c189339d152aee5d1c9c04a112e47974f4fd5
Dav420I <david.arutyunyan.97@bk.ru>
2022-09-20 19:20:18 +0300

    Figure commit 4.3

--- src/components/BoardComponents.tsx
+++ src/components/BoardComponents.tsx
@@ -11,14 +11,24 @@ interface BoardProps {
 const BoardComponents: FC<BoardProps> = ({board, setBoard }) => {
 
     const [selectedCell, setSelectedCell] = useState<Cell | null>(null )
+
+    function click(cell: Cell) {
+        if (cell.figure){
+            setSelectedCell(cell);
+        }
+
+    }
+
     return (
         <div className='board'>
             {board.cells.map((row, index) =>
                 <React.Fragment key={index}>
                     {row.map(cell =>
                         <CellComponents
+                            click={click}
                             cell={cell}
                             key={cell.id}
+                            selected={cell.x === selectedCell?.x && cell.y === selectedCell?.y}
                         />
                     )}
                 </React.Fragment>
